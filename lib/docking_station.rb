3fb5100375558ad2c93efd5be97df5398f8d6146
Ed <lookatemail@gmail.com>
2020-04-16 16:31:24 +0100

    chalange 11 done

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,22 +4,25 @@ class DockingStation
   attr_reader :bike 
 
   def release_bike
-    Bike.new
+    # Bike.new
+    fail 'No bikes available' unless @bike
+    @bike
   end
 
   def dock(bike)
-     @bike = bike
+    @bike = bike
   end
+
 end
 
-station1 = DockingStation.new
-bike1 = Bike.new
-station1.dock(bike1)
+# station1 = DockingStation.new
+# bike1 = Bike.new
+# station1.dock(bike1)
 
-read = DockingStation.new
+# read = DockingStation.new
 
-# p read.dock("Boris bike")
-# p read.bike
+# # p read.dock("Boris bike")
+# # p read.bike
 
-bike = station1.release_bike
-p bike.working? == true
+# bike = station1.release_bike
+# p bike.working? == true
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -11,6 +11,27 @@ describe DockingStation do
     expect(bike).to be_working
   end
 
-  it { is_expected.to respond_to(:dock).with(1).argument }
+  # it { is_expected.to respond_to(:dock).with(1).argument }
+
+  # it { is_expected.to respond_to(:bike) }
+
+  it 'dock something ' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
+
+  describe '#release_bike' do
+    it 'raises an error when there are no bikes available' do
+      # Let's not dock a bike first:
+      # remember subject == DockingStation.new
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
+  end
 
 end
